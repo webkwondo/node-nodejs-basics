6b04f1681cd8382a5c7463ebebd675e3ca67d9c4
Elena Markova <webkwondo.com@gmail.com>
2022-06-05 13:39:47 +0300

    feat: implement hash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import { createHash } from 'crypto';
+import { getDirName, join } from '../fs/getPath.js';
+import { readFile } from 'fs/promises';
+
+const __dirname = getDirName(import.meta.url);
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const fileName = 'fileToCalculateHashFor.txt';
+  const filePath = join(__dirname, folderName, fileName);
+
+  const fileBuffer = await readFile(filePath);
+  const hash = createHash('sha256');
+  hash.update(fileBuffer);
+
+  const hex = hash.digest('hex');
+
+  console.log(hex);
+
+  return hex;
 };
+
+calculateHash();
