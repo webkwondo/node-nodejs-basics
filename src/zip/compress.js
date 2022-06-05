9dd2847d78dbcc1eb2cc6f171ad6e93e8b89b284
Elena Markova <webkwondo.com@gmail.com>
2022-06-05 13:42:08 +0300

    feat: implement zip functions

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,34 @@
+import { createGzip } from 'zlib';
+import { pipeline } from 'stream';
+import { promisify } from 'util';
+import { unlink } from 'fs/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { getDirName, join } from '../fs/getPath.js';
+
+const __dirname = getDirName(import.meta.url);
+const pipelinePromise = promisify(pipeline);
+
 export const compress = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const fileName = 'fileToCompress.txt';
+  const filePath = join(__dirname, folderName, fileName);
+  const archiveName = 'archive.gz';
+  const archivePath = join(__dirname, folderName, archiveName);
+
+  const doGzip = async (input, output) => {
+    const gzip = createGzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    await pipelinePromise(source, gzip, destination);
+  };
+
+  try {
+    await doGzip(filePath, archivePath);
+    await unlink(filePath);
+  } catch (error) {
+    console.error('Operation failed: ', error);
+    process.exitCode = 1;
+  }
 };
+
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,34 @@
+import { createGunzip } from 'zlib';
+import { promisify } from 'util';
+import { pipeline } from 'stream';
+import { unlink } from 'fs/promises';
+import { createReadStream, createWriteStream } from 'fs';
+import { getDirName, join } from '../fs/getPath.js';
+
+const __dirname = getDirName(import.meta.url);
+const pipelinePromise = promisify(pipeline);
+
 export const decompress = async () => {
-    // Write your code here 
+  const folderName = 'files';
+  const fileName = 'fileToCompress.txt';
+  const filePath = join(__dirname, folderName, fileName);
+  const archiveName = 'archive.gz';
+  const archivePath = join(__dirname, folderName, archiveName);
+
+  const doGunzip = async (input, output) => {
+    const gunzip = createGunzip();
+    const source = createReadStream(input);
+    const destination = createWriteStream(output);
+    await pipelinePromise(source, gunzip, destination);
+  };
+
+  try {
+    await doGunzip(archivePath, filePath);
+    await unlink(archivePath);
+  } catch (error) {
+    console.error('Operation failed: ', error);
+    process.exitCode = 1;
+  }
 };
+
+decompress();
