655598c0399f114ab01bb6d1c7238f1570ef690e
Elena Markova <webkwondo.com@gmail.com>
2022-06-05 13:35:36 +0300

    feat: implement cli functions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,19 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+  let result = '';
+
+  for (let i = 0; i < args.length; i++) {
+    const propName = args[i];
+    let val = args[i + 1];
+    if (propName.startsWith('--') && val && !val.startsWith('--')) {
+      const str = propName + ' is ' + val;
+      result = (result) ? result + ', ' + str : result + str;
+    }
+  }
+
+  if (result) {
+    console.log(result);
+  }
 };
+
+parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,17 @@
 export const parseEnv = () => {
-    // Write your code here 
+  const envObj = process.env;
+  let result = '';
+
+  for (const [key, value] of Object.entries(envObj)) {
+    if (key.startsWith('RSS_')) {
+      const str = key + '=' + value;
+      result = (result) ? result + '; ' + str : result + str;
+    }
+  }
+
+  if (result) {
+    console.log(result);
+  }
 };
+
+parseEnv();
