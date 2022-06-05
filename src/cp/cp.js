a4d46049b654af89c19abea37035a3d0a5af8553
Elena Markova <webkwondo.com@gmail.com>
2022-06-05 13:47:27 +0300

    feat: implement child processes

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,20 @@
+import { spawn } from 'child_process';
+import { getDirName, join } from '../fs/getPath.js';
+
+const __dirname = getDirName(import.meta.url);
+
 export const spawnChildProcess = async (args) => {
-    // Write your code here
+  const folderName = 'files';
+  const fileName = 'script.js';
+  const filePath = join(__dirname, folderName, fileName);
+
+  const child = spawn('node', [filePath, ...args]);
+
+  process.stdin.pipe(child.stdin);
+
+  child.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
 };
+
+spawnChildProcess(['a1', 'a2', 'a3']);
