cc2b4772972f7396a7dfebfee75f1532ab07fc6f
Elena Markova <webkwondo.com@gmail.com>
2022-06-05 13:46:31 +0300

    feat: implement worker threads

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,14 @@
+import { parentPort, workerData }  from 'worker_threads';
+
+const number = workerData;
+
 // n should be received from main thread
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  // This function sends result of nthFibonacci computations to main thread
+  const result = nthFibonacci(number);
+  parentPort.postMessage(result);
 };
+
+sendResult();
